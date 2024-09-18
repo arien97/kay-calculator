63a9032f8a01aa848f56e1d14ca910958c70771b
arien <aramin@bu.edu>
2024-09-18 19:16:24 -0400

    created buttons for calculator and textfield

--- app/src/main/java/com/example/hw2calculator/MainActivity.kt
+++ app/src/main/java/com/example/hw2calculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,13 +13,100 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hw2calculator.ui.theme.HW2CalculatorTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            Calculator()
+        }
+    }
+}
 
+@Composable
+fun Calculator() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "1")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "2")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "3")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "+")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "-")
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "4")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "5")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "6")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "*")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "/")
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "7")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "8")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "9")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(2f)) {
+                Text(text = "sqrt")
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(2f)) {
+                Text(text = "0")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = ".")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(2f)) {
+                Text(text = "=")
+            }
         }
     }
 }
